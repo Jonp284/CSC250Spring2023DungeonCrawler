018a40205da8a7a45d4417e334ae948874e80406
Jonp284 <129097982+Jonp284@users.noreply.github.com>
2023-04-05 19:16:53 -0500

    HW # 11

--- DungeonCrawler/Assets/Scripts/DungeonSetup.cs
+++ DungeonCrawler/Assets/Scripts/DungeonSetup.cs
@@ -8,6 +8,8 @@ public class DungeonSetup : MonoBehaviour
     public GameObject southExit;
     public GameObject eastExit;
     public GameObject westExit;
+    public GameObject CenterPoint;
+    public Dictionary<GameObject, ExitDirection> ExitDirections = new Dictionary<GameObject, ExitDirection>();
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,11 @@ public class DungeonSetup : MonoBehaviour
         this.southExit.SetActive(true);
         this.eastExit.SetActive(true);
         this.westExit.SetActive(true);
+
+        ExitDirections.Add(northExit, ExitDirection.North);
+        ExitDirections.Add(southExit, ExitDirection.South);
+        ExitDirections.Add(eastExit, ExitDirection.East);
+        ExitDirections.Add(westExit, ExitDirection.West);
     }
 
     // Update is called once per frame
--- DungeonCrawler/Assets/Scripts/PlayerController.cs
+++ DungeonCrawler/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+public enum ExitDirection {North, South, East, West};
+
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
     private bool playerMovement = false;
+    
+    
 
 
     // Start is called before the first frame update
@@ -16,47 +21,78 @@ public class PlayerController : MonoBehaviour
     {
         this.rb = this.GetComponent<Rigidbody>();
         print(MasterData.count);
+
+        ExitDirection exitDirection = ExitDirection.North;
+
+        if(MasterData.count > 0)
+        {
+            GameObject previousExit = GameObject.Find("Exit" + (MasterData.count - 1));
+            exitDirection = DungeonSetup.ExitDirections[previousExit];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!playerMovement)
+        Vector3 movementDirection = Vector3.zero;
+        DungeonSetup dungeonSetup = FindObjectOfType<DungeonSetup>();
+        
+        switch (exitDirection)
         {
-            if(Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                playerMovement = true;
-                this.rb.AddForce(this.northExit.transform.position * movementSpeed);
-                MasterData.count++;
-                SceneManager.LoadScene("DungeonRoom1.5");
+            case ExitDirection.North:
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    playerMovement = true;
+                    movementDirection = this.northExit.transform.position - this.transform.position;
+                    MasterData.count++;
+                    SceneManager.LoadScene("DungeonRoom1.5");
+                }
+                break;
+            case ExitDirection.South:
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    playerMovement = true;
+                    movementDirection = this.southExit.transform.position - this.transform.position;
+                    MasterData.count++;
+                    SceneManager.LoadScene("DungeonRoom3");
+                }
+                break;
+            case ExitDirection.East:
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    playerMovement = true;
+                    movementDirection = this.eastExit.transform.position - this.transform.position;
+                    MasterData.count++;
+                    SceneManager.LoadScene("DungeonRoom5");
+                }
+                break;
+            case ExitDirection.West:
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    playerMovement = true;
+                    movementDirection = this.westExit.transform.position - this.transform.position;
+                    MasterData.count++;
+                    SceneManager.LoadScene("DungeonRoom2");
+                }
+                break;
+        }
+
+        if (playerMovement)
+        {
+            this.rb.AddForce(movementDirection.normalized * movementSpeed);
+        }
             }
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                playerMovement = true;
-                this.rb.AddForce(this.westExit.transform.position * movementSpeed);
-                MasterData.count++;
-                SceneManager.LoadScene("DungeonRoom2");
-            }
-            if(Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                playerMovement = true;
-                this.rb.AddForce(this.southExit.transform.position * movementSpeed);
-                MasterData.count++;
-                SceneManager.LoadScene("DungeonRoom3");
-            }
-            if(Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                playerMovement = true;
-                this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
-                MasterData.count++;
-                SceneManager.LoadScene("DungeonRoom5");
-            }
-        } 
-    }
 
     void OnCollisionEnter(Collision collision)
     {
-        playerMovement = false;
+        if (collision.gameObject == DungeonSetup.CenterPoint)
+        {
+            playerMovement = false;
+            this.rb.velocity = Vector3.zero;
+            this.rb.angularVelocity = Vector3.zero;
+        }
     }
+    
+
     
 }
